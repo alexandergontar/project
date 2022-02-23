847e77378d4df96b0ac7a246a12d824ab19d5c7b
agontar <agontar@yandex.ru>
2022-02-23 18:31:56 +0300

    code comments added

--- homework/lesson_1/lesson_1/Program.cs
+++ homework/lesson_1/lesson_1/Program.cs
@@ -1,19 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+/* This is the first
+ * Console input/output 
+ * example.
+ */ 
 namespace lesson_1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введитя ваше имя:");
-            string guestName = Console.ReadLine();
-            Console.WriteLine($"Сейчас [{DateTime.Now}], ваше Имя: {guestName}.");
-            Console.ReadKey();
+            Console.Write("Введитя ваше имя: "); // prompt to put user's name
+            string guestName = Console.ReadLine(); // put string in variable
+            Console.WriteLine($"Привет, {guestName}, сегодня [{DateTime.Now}], "); // console output
+         
+            Console.ReadKey(); // wait for any key to be pressed
         }
     }
 }
