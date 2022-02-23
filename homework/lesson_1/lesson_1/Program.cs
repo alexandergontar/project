fdc6666d891324ace058d0b39b01f4e03a648dfb
agontar <agontar@yandex.ru>
2022-02-23 16:25:21 +0300

    lesson-1 homework started

---
+++ homework/lesson_1/lesson_1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введитя ваше имя:");
+            string guestName = Console.ReadLine();
+            Console.WriteLine($"Сейчас [{DateTime.Now}], ваше Имя: {guestName}.");
+            Console.ReadKey();
+        }
+    }
+}
