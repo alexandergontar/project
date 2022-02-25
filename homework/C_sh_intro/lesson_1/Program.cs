40b783b65718e3f264a1bd040a8ca1904b7c07b8
agontar <agontar@yandex.ru>
2022-02-25 08:48:24 +0300

    first lesson homework

--- homework/C_sh_intro/lesson_1/Program.cs
+++ homework/C_sh_intro/lesson_1/Program.cs
@@ -11,7 +11,7 @@ namespace lesson_1
         {
             Console.Write("Введитя ваше имя: "); // prompt to put user's name
             string guestName = Console.ReadLine(); // put string in variable
-            Console.WriteLine($"Привет, {guestName}, сегодня [{DateTime.Now}], "); // console output         
+            Console.WriteLine($"Привет, {guestName}, сегодня [{DateTime.Now}] "); // console output         
             Console.ReadKey(); // wait for any key to be pressed
         }
     }
