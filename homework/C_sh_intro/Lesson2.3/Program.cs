782522082c1db5315f83c16987b06415170de207
agontar <agontar@yandex.ru>
2022-03-02 10:34:00 +0300

    odd - even number feature added

--- homework/C_sh_intro/Lesson2.3/Program.cs
+++ homework/C_sh_intro/Lesson2.3/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+/*
+ determine if number is odd or even
+ */
 
 namespace Lesson2._3
 {
