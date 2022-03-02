fa689969b4d91ad83689cae72f7821f4e6efe5d0
agontar <agontar@yandex.ru>
2022-03-02 09:06:36 +0300

    average day temp calculation added

--- homework/C_sh_intro/Lesson2.1/Program.cs
+++ homework/C_sh_intro/Lesson2.1/Program.cs
@@ -1,22 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
+/* 
+ * Average temp calculation
+ */
 namespace Lesson2._1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            // temp input
             Console.Write("Введите максимальную суточную температуру: ");
             double tempMax = Convert.ToDouble(Console.ReadLine());
             Console.Write("Введите минимальную суточную температуру: ");
             double tempMin = Convert.ToDouble(Console.ReadLine());
             double tempAvg = (tempMax + tempMin) / 2;
             Console.WriteLine($"Среднесуточная температура: {tempAvg} градусов.");
-            if (tempAvg > 0)
+            if (tempAvg > 0) // if temp > 0 and it is winter time display "Дождливая зима" 
             {                                  ;
                 int monthNumber = DateTime.Now.Month;
                 if (monthNumber == 1 || monthNumber == 2 || monthNumber == 12)
