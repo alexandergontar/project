a51ff0a839b89eb2f899ba90a5948f8ece318bcc
agontar <agontar@yandex.ru>
2022-03-22 09:26:45 +0300

    enter/display user list

---
+++ homework/C_sh_intro/Lesson4.1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+/*
+ enter/display user list
+ */
+
+namespace Lesson4._1
+{
+    class Program
+    {
+
+        static string GetFullName(string firstname, string lastName, string patronymic) 
+        {
+
+            string fullName = $" {firstname} {patronymic} {lastName}";
+            return fullName;
+        }
+
+        static string InputFullName() 
+        {
+            Console.Write("Введите имя: ");
+            string firstName = Console.ReadLine();
+            Console.Write("Введите отчество: ");
+            string patronymic = Console.ReadLine();
+            Console.Write("Введите фамлмя: ");
+            string lastName = Console.ReadLine();
+            string fullname = GetFullName(firstName, lastName, patronymic);
+            Console.WriteLine("ФИО: "+ fullname);
+            Console.WriteLine("Нажмите любую клавишу");
+            Console.ReadKey();
+            return fullname;
+        }
+
+        static string[] userFullNames; 
+        static void Main(string[] args)
+        {
+
+            Console.Write("Введите число вносимых пользователей: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(number);
+            userFullNames = new string[number];
+            for (int i = 0; i < userFullNames.Length; i++)
+            {
+              userFullNames[i] = InputFullName();
+            }
+            Console.WriteLine("---- Весь список ----");
+            for (int i = 0; i < userFullNames.Length; i++)
+            {
+                Console.WriteLine((i+1) +". ФИО: "+ userFullNames[i]);
+            }
+ 
+            Console.ReadKey();
+
+        }
+    }
+}
