d3ef67af9d0ebb63b2fbad7d88342b32a3a08c4e
agontar <agontar@yandex.ru>
2022-03-22 09:45:30 +0300

    parsing and addition of space separated numbers

---
+++ homework/C_sh_intro/Lesson4.2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+/*
+ parse and add up space separated numbers
+ */
+
+namespace Lesson4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите ряд целых чисел, разделенных пробелом: ");
+            string inputString = Console.ReadLine();
+            inputString += " ";
+            string temp = String.Empty;
+            int count = 0;
+            int[] numbAarray = new int[10];
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                char c = inputString[i];
+                if (Char.IsDigit(c))
+                {
+                    temp += c;
+                }
+                else if (temp.Length > 0)
+                {
+                    Console.WriteLine(temp);
+                    numbAarray[count] = Convert.ToInt32(temp);
+                    temp = String.Empty;
+                    count++;
+                }
+            }
+
+            int sum = 0;
+            for (int i = 0; i < numbAarray.Length; i++)
+            {
+                sum += numbAarray[i];
+            }
+            Console.WriteLine("Сумма чисел: " + sum.ToString());
+            Console.ReadKey();
+        }
+    }
+}
