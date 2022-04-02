72b762413942624ac9affce5488462a3248afe78
agontar <agontar@yandex.ru>
2022-04-02 18:31:05 +0300

    get current dir entries path tree by loop and recursion

---
+++ homework/C_sh_intro/Lesson5.4/FileSystemInfo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace Lesson5._4
+{
+    public static class FileSystemInfo
+    {
+        public static void GetTree(string path, string root, ref string content)
+        {
+            if (path == root)
+            {
+                return;
+            }
+            if (Directory.GetParent(path) != null)
+            {
+                path = Directory.GetParent(path).ToString();
+                Console.WriteLine(path);
+                content += path + "\n";
+                GetTree(path, root, ref content);
+            }
+        }
+
+        public static void GetEntries(string currDir, ref string content)
+        {
+            string[] entries = Directory.GetFileSystemEntries(currDir,
+          "*", SearchOption.AllDirectories);
+            for (int i = 0; i < entries.Length; i++)
+            {
+                Console.WriteLine(entries[i]);
+                content += entries[i] + "\n";
+            }
+        }
+    }
+}
---
+++ homework/C_sh_intro/Lesson5.4/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+/*
+ get current dir entries path tree by loop and recursion
+ */
+namespace Lesson5._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = "tree.txt";
+            string content = String.Empty;
+            string currDir = Environment.CurrentDirectory;
+            string root = currDir.Substring(0, 3); // e.g "C:\" or "D:\"
+            Console.WriteLine("--Запись в файл: " + filename);
+            FileSystemInfo.GetEntries(currDir, ref content); //get current dir enrties
+            Console.WriteLine(currDir);
+            content += currDir + "\n";
+            while (currDir != root) //get tree by loop
+            {
+                try
+                {
+                    if (Directory.GetParent(currDir) != null)
+                    {
+                        currDir = Directory.GetParent(currDir).ToString();
+                        Console.WriteLine(currDir);
+                        content += currDir + "\n";
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("End");
+                }
+            }
+            File.WriteAllText(filename, content);
+            filename = "recurtree.txt";
+            content = String.Empty;
+            currDir = Environment.CurrentDirectory;
+            Console.WriteLine("Теперь путем рекурсии, нажмите любую клавишу:");
+            Console.ReadKey();
+            Console.WriteLine("--Запись в файл: " + filename);
+            FileSystemInfo.GetEntries(currDir, ref content); // get current dir entries
+            Console.WriteLine(currDir);
+            content += currDir + "\n";
+            FileSystemInfo.GetTree(currDir, root, ref content); // get tree by recursive method
+            File.WriteAllText(filename, content);
+            Console.ReadKey();
+        }
+    }
+}
