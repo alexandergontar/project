f3f941c4d86aa5bc642296d85f0e0fb275a41912
agontar <agontar@yandex.ru>
2022-04-02 18:03:09 +0300

    key board bytes entries save to binary file function

---
+++ homework/C_sh_intro/Lesson-5.3/ParseNumbers.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson_5._3
+{
+    public static class ParseNumbers
+    {
+        public static byte[] ParseBinary(string inputString)
+        {
+            inputString += " ";
+            string temp = String.Empty;
+            int count = 0;
+            byte[] byteAarray = new byte[100];
+
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                char c = inputString[i];
+                if (Char.IsDigit(c))
+                {
+                    temp += c;
+                }
+                else if (temp.Length > 0)
+                {
+                    int n = Convert.ToInt32(temp);
+                    if (n > 255 || n < 0)
+                    {
+                        Console.WriteLine("Числа должны быть в интервале: 0...255");
+                        Console.ReadKey();
+                        return new byte[0];
+                    }
+                    byteAarray[count] = Convert.ToByte(temp);
+                    temp = String.Empty;
+                    count++;
+                }
+            }
+
+            byte[] writeBinary = new byte[count];
+            for (int i = 0; i < count; i++)
+            {
+                writeBinary[i] = byteAarray[i];
+            }
+
+            return writeBinary;
+        }
+    }
+}
---
+++ homework/C_sh_intro/Lesson-5.3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+namespace Lesson_5._3
+    /*
+     key board bytes entries save to binary file
+     */
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = "bytes.bin";            
+            Console.Write("Введите целые числа в интервале 0...255, разделенные пробелом: ");
+            string inputString = Console.ReadLine();
+            byte[] writeBinary = ParseNumbers.ParseBinary(inputString);
+            if (writeBinary.Length == 0)
+            {
+                Console.WriteLine("Ошибка ввода, нажмите любую клавишу для завершения.");
+                Console.ReadKey();
+                return;
+            }
+            File.WriteAllBytes(fileName, writeBinary);
+            byte[] fromFile = File.ReadAllBytes(fileName);
+            Console.WriteLine("Запись произведена, содержимое бинарного файла:");
+            for (int i = 0; i < fromFile.Length; i++)
+            {
+                Console.WriteLine(fromFile[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
