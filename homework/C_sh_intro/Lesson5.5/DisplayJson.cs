3d8500d9c9c1e6f6de60b920d0712f7970b98a81
agontar <agontar@yandex.ru>
2022-04-03 08:36:31 +0300

    ToDo list feature added

---
+++ homework/C_sh_intro/Lesson5.5/DisplayJson.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Text.Json;
+
+namespace Lesson5._5
+{
+    public static class DisplayJson
+    {
+        public static void DisplayContent(string fileName, out string json, out ToDo[] items)
+        {
+            json = File.ReadAllText(fileName);
+            items = JsonSerializer.Deserialize<ToDo[]>(json);
+            for (int i = 0; i < items.Length; i++)
+            {
+                string check = "   ";
+                if (items[i].IsDone) check = "[x]";
+                Console.Write($" {i + 1} {check} {items[i].Title} ");
+                Console.WriteLine();
+            }
+        }
+        public static void FillContent(string fileName, out string json, out ToDo[] items)
+        {
+            items = new ToDo[10];
+            items[0] = new ToDo("homework", false);
+            items[1] = new ToDo("cleaning", false);
+            items[2] = new ToDo("maintenance", false);
+            items[3] = new ToDo("shopping", false);
+            items[4] = new ToDo("hiking", false);
+            items[5] = new ToDo("reading", false);
+            items[6] = new ToDo("delivery", false);
+            items[7] = new ToDo("shipment", false);
+            items[8] = new ToDo("calling customers", false);
+            items[9] = new ToDo("reporting", false);
+            json = JsonSerializer.Serialize(items);
+            File.WriteAllText(fileName, json);
+        }
+
+    }
+}
---
+++ homework/C_sh_intro/Lesson5.5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+namespace Lesson5._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = "tasks.json";
+            string json;
+            ToDo[] items;
+            if (File.Exists(fileName))
+            {
+                DisplayJson.DisplayContent(fileName, out json, out items);
+            }
+            else
+            {
+                DisplayJson.FillContent(fileName, out json, out items);
+                DisplayJson.DisplayContent(fileName, out json, out items);
+            }
+            Console.Write("Введите номер завершенного пункта или 0 для выхода: ");
+            int n;
+            try
+            {
+                n = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                n = 0;
+            }
+            if (n == 0) return;
+            items[n - 1].IsDone = true;
+            json = JsonSerializer.Serialize(items);
+            File.WriteAllText(fileName, json);
+            DisplayJson.DisplayContent(fileName, out json, out items);
+            Console.WriteLine("Press any key");
+            Console.ReadKey();
+        }
+    }
+}
