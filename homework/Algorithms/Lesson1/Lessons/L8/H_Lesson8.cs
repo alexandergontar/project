229a4d42d3b0ff1842d4a5001207a469f01114c5
agontar <agontar@yandex.ru>
2022-06-07 08:48:32 +0300

    counting search draft

---
+++ homework/Algorithms/Lesson1/Lessons/L8/H_Lesson8.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lessons.L8
+{
+    public class H_Lesson8 : ILesson
+    {
+        public void RunLesson()
+        {
+            Console.WriteLine("===== Урок 8 ======");
+            Console.ReadKey();
+        }
+    }
+}
